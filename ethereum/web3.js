22320513e129a4ea9d616546df6347a1e1dc3fa4
Anjani Raj <anjani.madhu97@gmail.com>
2020-09-18 11:37:25 +0530

    index components

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -1,5 +1,15 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(window.ethereum)
+let web3;
+
+if (typeof window !== 'undefined' && typeof window.ethereum !== 'undefined') {
+    //check if code in running in a browser and has metamask in it
+    web3 = new Web3(window.ethereum);
+
+} else {
+    //we are not on browser (on server) or the user is not running metamask
+    const provider = new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/de72ca31dd2544fd8c1b4fb64dfefc8f');
+    web3 = new Web3(provider);
+}
 
 export default web3;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,51 @@
-import React from 'react';
+import React, {Component} from 'react';
+import factory from '../ethereum/factory';
+import {Card, Button} from 'semantic-ui-react';
+import Layout from "../components/Layout";
 
-export default () => {
-    return <h1>This is campaign</h1>;
-};
+class CampaignIndex extends Component {
 
+    static async getInitialProps() {
+        const campaigns = await factory.methods.getDeployedCampaigns().call();
+        return {campaigns};
+    }
 
+    renderCampaigns() {
+        const items = this.props.campaigns.map(address => {
+            return {
+                header: address,
+                description: <a>View Campaign</a>,
+                fluid: true
+            };
+        });
+
+        return <Card.Group items={items}/>;
+    }
+
+    render() {
+        return (
+            <Layout>
+                <div>
+                    <link
+                        async
+                        rel="stylesheet"
+                        href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.css"
+                    />
+                    <script
+                        async
+                        src="//cdn.jsdelivr.net/npm/semantic-ui@2.4.1/dist/semantic.min.js"
+                    ></script>
+                    <h3>Open Campaigns</h3>
+                    {this.renderCampaigns()}
+                    <Button
+                        content="Create Campaign"
+                        icon="add"
+                        primary
+                    />
+                </div>
+            </Layout>
+        );
+    }
+}
+
+export default CampaignIndex;
